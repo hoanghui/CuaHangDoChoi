a9eaf1b4dac746588618fa481530f9df0dca9eaa
= <=>
2019-10-21 22:10:50 +0700

    Chinh sua Form dang nhap

--- CuaHangDoChoi/CuaHangDoChoi/Form1.Designer.cs
+++ CuaHangDoChoi/CuaHangDoChoi/Form1.Designer.cs
@@ -63,6 +63,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(238, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(310, 55);
@@ -73,6 +74,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btSignin);
