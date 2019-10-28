6e1b251e653d4f2ac1c08689c0252a8a426148f7
= <=>
2019-10-28 19:28:01 +0700

    Tao form THONG KE

---
+++ CuaHangDoChoi/CuaHangDoChoi/ThongKe.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CuaHangDoChoi
+{
+    partial class ThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThongKe));
+            this.pbSearch = new System.Windows.Forms.PictureBox();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.lvDanhSachNhanVien = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btXoaNV = new System.Windows.Forms.Button();
+            this.btThemNV = new System.Windows.Forms.Button();
+            this.btDangXuat = new System.Windows.Forms.Button();
+            this.btThongKe = new System.Windows.Forms.Button();
+            this.btListHangHoa = new System.Windows.Forms.Button();
+            this.bbtListKhachHang = new System.Windows.Forms.Button();
+            this.btListNhanVien = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSearch
+            // 
+            this.pbSearch.Image = ((System.Drawing.Image)(resources.GetObject("pbSearch.Image")));
+            this.pbSearch.Location = new System.Drawing.Point(713, 81);
+            this.pbSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.pbSearch.Name = "pbSearch";
+            this.pbSearch.Size = new System.Drawing.Size(28, 31);
+            this.pbSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSearch.TabIndex = 43;
+            this.pbSearch.TabStop = false;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(504, 81);
+            this.txtTimKiem.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTimKiem.Multiline = true;
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(205, 32);
+            this.txtTimKiem.TabIndex = 42;
+            // 
+            // lvDanhSachNhanVien
+            // 
+            this.lvDanhSachNhanVien.HideSelection = false;
+            this.lvDanhSachNhanVien.Location = new System.Drawing.Point(204, 117);
+            this.lvDanhSachNhanVien.Margin = new System.Windows.Forms.Padding(2);
+            this.lvDanhSachNhanVien.Name = "lvDanhSachNhanVien";
+            this.lvDanhSachNhanVien.Size = new System.Drawing.Size(538, 266);
+            this.lvDanhSachNhanVien.TabIndex = 41;
+            this.lvDanhSachNhanVien.UseCompatibleStateImageBehavior = false;
+            this.lvDanhSachNhanVien.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 64);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "H2K TOY STORE";
+            // 
+            // btXoaNV
+            // 
+            this.btXoaNV.Location = new System.Drawing.Point(648, 387);
+            this.btXoaNV.Margin = new System.Windows.Forms.Padding(2);
+            this.btXoaNV.Name = "btXoaNV";
+            this.btXoaNV.Size = new System.Drawing.Size(93, 46);
+            this.btXoaNV.TabIndex = 33;
+            this.btXoaNV.Text = "Xóa nhân viên";
+            this.btXoaNV.UseVisualStyleBackColor = true;
+            // 
+            // btThemNV
+            // 
+            this.btThemNV.Location = new System.Drawing.Point(546, 387);
+            this.btThemNV.Margin = new System.Windows.Forms.Padding(2);
+            this.btThemNV.Name = "btThemNV";
+            this.btThemNV.Size = new System.Drawing.Size(98, 46);
+            this.btThemNV.TabIndex = 34;
+            this.btThemNV.Text = "Thêm nhân viên";
+            this.btThemNV.UseVisualStyleBackColor = true;
+            // 
+            // btDangXuat
+            // 
+            this.btDangXuat.Location = new System.Drawing.Point(58, 426);
+            this.btDangXuat.Margin = new System.Windows.Forms.Padding(2);
+            this.btDangXuat.Name = "btDangXuat";
+            this.btDangXuat.Size = new System.Drawing.Size(142, 46);
+            this.btDangXuat.TabIndex = 35;
+            this.btDangXuat.Text = "Đăng xuất";
+            this.btDangXuat.UseVisualStyleBackColor = true;
+            // 
+            // btThongKe
+            // 
+            this.btThongKe.Enabled = false;
+            this.btThongKe.Location = new System.Drawing.Point(58, 283);
+            this.btThongKe.Margin = new System.Windows.Forms.Padding(2);
+            this.btThongKe.Name = "btThongKe";
+            this.btThongKe.Size = new System.Drawing.Size(142, 50);
+            this.btThongKe.TabIndex = 36;
+            this.btThongKe.Text = "Thống kê";
+            this.btThongKe.UseVisualStyleBackColor = true;
+            // 
+            // btListHangHoa
+            // 
+            this.btListHangHoa.Location = new System.Drawing.Point(58, 227);
+            this.btListHangHoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btListHangHoa.Name = "btListHangHoa";
+            this.btListHangHoa.Size = new System.Drawing.Size(142, 50);
+            this.btListHangHoa.TabIndex = 37;
+            this.btListHangHoa.Text = "Danh sách hàng hóa";
+            this.btListHangHoa.UseVisualStyleBackColor = true;
+            // 
+            // bbtListKhachHang
+            // 
+            this.bbtListKhachHang.Location = new System.Drawing.Point(58, 172);
+            this.bbtListKhachHang.Margin = new System.Windows.Forms.Padding(2);
+            this.bbtListKhachHang.Name = "bbtListKhachHang";
+            this.bbtListKhachHang.Size = new System.Drawing.Size(142, 50);
+            this.bbtListKhachHang.TabIndex = 38;
+            this.bbtListKhachHang.Text = "Danh sách khách hàng";
+            this.bbtListKhachHang.UseVisualStyleBackColor = true;
+            // 
+            // btListNhanVien
+            // 
+            this.btListNhanVien.Location = new System.Drawing.Point(58, 117);
+            this.btListNhanVien.Margin = new System.Windows.Forms.Padding(2);
+            this.btListNhanVien.Name = "btListNhanVien";
+            this.btListNhanVien.Size = new System.Drawing.Size(142, 50);
+            this.btListNhanVien.TabIndex = 39;
+            this.btListNhanVien.Text = "Danh sách nhân viên";
+            this.btListNhanVien.UseVisualStyleBackColor = true;
+            // 
+            // ThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.pbSearch);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.lvDanhSachNhanVien);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btXoaNV);
+            this.Controls.Add(this.btThemNV);
+            this.Controls.Add(this.btDangXuat);
+            this.Controls.Add(this.btThongKe);
+            this.Controls.Add(this.btListHangHoa);
+            this.Controls.Add(this.bbtListKhachHang);
+            this.Controls.Add(this.btListNhanVien);
+            this.Name = "ThongKe";
+            this.Text = "ThongKe";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSearch;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.ListView lvDanhSachNhanVien;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btXoaNV;
+        private System.Windows.Forms.Button btThemNV;
+        private System.Windows.Forms.Button btDangXuat;
+        private System.Windows.Forms.Button btThongKe;
+        private System.Windows.Forms.Button btListHangHoa;
+        private System.Windows.Forms.Button bbtListKhachHang;
+        private System.Windows.Forms.Button btListNhanVien;
+    }
+}
