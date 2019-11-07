90fe24d2ca525e1838b4befae3c6af6c3a6f500f
toidilootdao <hoanghuy159@gmail.com>
2019-11-07 15:16:04 +0700

    Them class SanPhamDAO vao DAO

---
+++ CuaHangDoChoi/DAO/SanPhamDAO.cs
@@ -0,0 +1,37 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class SanPhamDAO
+    {
+        //biến singleton, khởi tạo 1 lần duy nhất
+        private static SanPhamDAO instance;
+
+        public static SanPhamDAO Instance
+        {
+            get { if (instance == null) instance = new SanPhamDAO(); return instance; }
+            private set => instance = value;
+        }
+
+        private SanPhamDAO() { }
+
+        // đổ data vào 
+        public SanPham layMaKhachHang(string maKhachHang)
+        {
+            string query = "SELECT * FROM dbo.KhachHang WHERE maKhachHang= '" + maKhachHang + "'";
+            DataTable table = DataProvider.Instance.ExecuteQuery(query);
+            foreach (DataRow row in table.Rows)
+            {
+                return new SanPham(row);
+            }
+            return null;
+        }
+
+    }
+}
